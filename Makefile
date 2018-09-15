76d6a8463591a32aae55d3f74b7cb4044b3ec66a
Ryo Manzoku <manzoku.ryo@gmail.com>
2018-09-15 03:30:36 +0000

    schema管理

--- Makefile
+++ Makefile
@@ -9,5 +9,9 @@ run:
 build:
 	go build -o $(NAME) app.go
 
+schema:
+	scp schema.sql app40:~/schema.sql
+	ssh app40 "./schemalex schema.sql \"mysql://isucon:isucon@tcp(localhost:3306)/torb\""
+
 deploy: build
 	scp $(NAME) app1:/tmp/
--- schema.sql
+++ schema.sql
@@ -1,41 +1,47 @@
-CREATE TABLE IF NOT EXISTS users (
-    id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    nickname    VARCHAR(128) NOT NULL,
-    login_name  VARCHAR(128) NOT NULL,
-    pass_hash   VARCHAR(128) NOT NULL,
-    UNIQUE KEY login_name_uniq (login_name)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS events (
-    id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    title       VARCHAR(128)     NOT NULL,
-    public_fg   TINYINT(1)       NOT NULL,
-    closed_fg   TINYINT(1)       NOT NULL,
-    price       INTEGER UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `sheets` (
+`id` INT (10) UNSIGNED NOT NULL AUTO_INCREMENT,
+`rank` VARCHAR (128) NOT NULL,
+`num` INT (10) UNSIGNED NOT NULL,
+`price` INT (10) UNSIGNED NOT NULL,
+PRIMARY KEY (`id`),
+UNIQUE INDEX `rank_num_uniq` (`rank`, `num`)
+) DEFAULT CHARACTER SET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS sheets (
-    id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    `rank`      VARCHAR(128)     NOT NULL,
-    num         INTEGER UNSIGNED NOT NULL,
-    price       INTEGER UNSIGNED NOT NULL,
-    UNIQUE KEY rank_num_uniq (`rank`, num)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `users` (
+`id` INT (10) UNSIGNED NOT NULL AUTO_INCREMENT,
+`nickname` VARCHAR (128) NOT NULL,
+`login_name` VARCHAR (128) NOT NULL,
+`pass_hash` VARCHAR (128) NOT NULL,
+PRIMARY KEY (`id`),
+UNIQUE INDEX `login_name_uniq` (`login_name`)
+)  DEFAULT CHARACTER SET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS reservations (
-    id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    event_id    INTEGER UNSIGNED NOT NULL,
-    sheet_id    INTEGER UNSIGNED NOT NULL,
-    user_id     INTEGER UNSIGNED NOT NULL,
-    reserved_at DATETIME(6)      NOT NULL,
-    canceled_at DATETIME(6)      DEFAULT NULL,
-    KEY event_id_and_sheet_id_idx (event_id, sheet_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `administrators` (
+`id` INT (10) UNSIGNED NOT NULL AUTO_INCREMENT,
+`nickname` VARCHAR (128) NOT NULL,
+`login_name` VARCHAR (128) NOT NULL,
+`pass_hash` VARCHAR (128) NOT NULL,
+PRIMARY KEY (`id`),
+UNIQUE INDEX `login_name_uniq` (`login_name`)
+)  DEFAULT CHARACTER SET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS administrators (
-    id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    nickname    VARCHAR(128) NOT NULL,
-    login_name  VARCHAR(128) NOT NULL,
-    pass_hash   VARCHAR(128) NOT NULL,
-    UNIQUE KEY login_name_uniq (login_name)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `events` (
+`id` INT (10) UNSIGNED NOT NULL AUTO_INCREMENT,
+`title` VARCHAR (128) NOT NULL,
+`public_fg` TINYINT (1) NOT NULL,
+`closed_fg` TINYINT (1) NOT NULL,
+`price` INT (10) UNSIGNED NOT NULL,
+PRIMARY KEY (`id`)
+)  DEFAULT CHARACTER SET = utf8mb4;
+
+CREATE TABLE `reservations` (
+`id` INT (10) UNSIGNED NOT NULL AUTO_INCREMENT,
+`event_id` INT (10) UNSIGNED NOT NULL,
+`sheet_id` INT (10) UNSIGNED NOT NULL,
+`user_id` INT (10) UNSIGNED NOT NULL,
+`reserved_at` DATETIME (6) NOT NULL,
+`canceled_at` DATETIME (6) DEFAULT NULL,
+PRIMARY KEY (`id`),
+INDEX `event_id_and_sheet_id_idx` (`event_id`, `sheet_id`)
+)  DEFAULT CHARACTER SET = utf8mb4;
