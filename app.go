cbb8d666e435df4e681674b4228f451a7f01981b
ayumu83s <ayumu.itou.1125@gmail.com>
2018-09-15 13:41:50 +0900

    Rename method

--- app.go
+++ app.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/labstack/echo-contrib/session"
 	"html/template"
 	"io"
 	"log"
@@ -19,7 +20,6 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/sessions"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/middleware"
 )
 
@@ -221,7 +221,7 @@ func getEvents(all bool) ([]*Event, error) {
 	return events, nil
 }
 
-func getEvent2(eventID, loginUserID int64) (*Event, error) {
+func getEvent(eventID, loginUserID int64) (*Event, error) {
 	var event Event
 	if err := db.QueryRow("SELECT * FROM events WHERE id = ?", eventID).Scan(&event.ID, &event.Title, &event.PublicFg, &event.ClosedFg, &event.Price); err != nil {
 		return nil, err
@@ -277,51 +277,51 @@ func getEvent2(eventID, loginUserID int64) (*Event, error) {
 	return &event, nil
 }
 
-func getEvent(eventID, loginUserID int64) (*Event, error) {
-	var event Event
-	if err := db.QueryRow("SELECT * FROM events WHERE id = ?", eventID).Scan(&event.ID, &event.Title, &event.PublicFg, &event.ClosedFg, &event.Price); err != nil {
-		return nil, err
-	}
-	event.Sheets = map[string]*Sheets{
-		"S": &Sheets{},
-		"A": &Sheets{},
-		"B": &Sheets{},
-		"C": &Sheets{},
-	}
-
-	rows, err := db.Query("SELECT * FROM sheets ORDER BY `rank`, num")
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var sheet Sheet
-		if err := rows.Scan(&sheet.ID, &sheet.Rank, &sheet.Num, &sheet.Price); err != nil {
-			return nil, err
-		}
-		event.Sheets[sheet.Rank].Price = event.Price + sheet.Price
-		event.Total++
-		event.Sheets[sheet.Rank].Total++
-
-		var reservation Reservation
-		err := db.QueryRow("SELECT * FROM reservations WHERE event_id = ? AND sheet_id = ? AND canceled_at IS NULL GROUP BY event_id, sheet_id HAVING reserved_at = MIN(reserved_at)", event.ID, sheet.ID).Scan(&reservation.ID, &reservation.EventID, &reservation.SheetID, &reservation.UserID, &reservation.ReservedAt, &reservation.CanceledAt)
-		if err == nil {
-			sheet.Mine = reservation.UserID == loginUserID
-			sheet.Reserved = true
-			sheet.ReservedAtUnix = reservation.ReservedAt.Unix()
-		} else if err == sql.ErrNoRows {
-			event.Remains++
-			event.Sheets[sheet.Rank].Remains++
-		} else {
-			return nil, err
-		}
-
-		event.Sheets[sheet.Rank].Detail = append(event.Sheets[sheet.Rank].Detail, &sheet)
-	}
-
-	return &event, nil
-}
+//func getEvent(eventID, loginUserID int64) (*Event, error) {
+//	var event Event
+//	if err := db.QueryRow("SELECT * FROM events WHERE id = ?", eventID).Scan(&event.ID, &event.Title, &event.PublicFg, &event.ClosedFg, &event.Price); err != nil {
+//		return nil, err
+//	}
+//	event.Sheets = map[string]*Sheets{
+//		"S": &Sheets{},
+//		"A": &Sheets{},
+//		"B": &Sheets{},
+//		"C": &Sheets{},
+//	}
+//
+//	rows, err := db.Query("SELECT * FROM sheets ORDER BY `rank`, num")
+//	if err != nil {
+//		return nil, err
+//	}
+//	defer rows.Close()
+//
+//	for rows.Next() {
+//		var sheet Sheet
+//		if err := rows.Scan(&sheet.ID, &sheet.Rank, &sheet.Num, &sheet.Price); err != nil {
+//			return nil, err
+//		}
+//		event.Sheets[sheet.Rank].Price = event.Price + sheet.Price
+//		event.Total++
+//		event.Sheets[sheet.Rank].Total++
+//
+//		var reservation Reservation
+//		err := db.QueryRow("SELECT * FROM reservations WHERE event_id = ? AND sheet_id = ? AND canceled_at IS NULL GROUP BY event_id, sheet_id HAVING reserved_at = MIN(reserved_at)", event.ID, sheet.ID).Scan(&reservation.ID, &reservation.EventID, &reservation.SheetID, &reservation.UserID, &reservation.ReservedAt, &reservation.CanceledAt)
+//		if err == nil {
+//			sheet.Mine = reservation.UserID == loginUserID
+//			sheet.Reserved = true
+//			sheet.ReservedAtUnix = reservation.ReservedAt.Unix()
+//		} else if err == sql.ErrNoRows {
+//			event.Remains++
+//			event.Sheets[sheet.Rank].Remains++
+//		} else {
+//			return nil, err
+//		}
+//
+//		event.Sheets[sheet.Rank].Detail = append(event.Sheets[sheet.Rank].Detail, &sheet)
+//	}
+//
+//	return &event, nil
+//}
 
 func sanitizeEvent(e *Event) *Event {
 	sanitized := *e
